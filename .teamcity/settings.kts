cf31e1e14ef7d1d9f07800cae460a78f218e0e99
admin <admin@TeamCity>
2025-01-09 16:31:54 +0100

    TeamCity change in 'AWS Example' project: Updated cloud profile 'amazon-2' in project 'project31'

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -75,7 +75,7 @@ project {
             connectionId = "AmazonWebServicesAws_2"
         }
         amazonEC2CloudImage {
-            id = "PROJECT_EXT_31"
+            id = "PROJECT_EXT_33"
             profileId = "amazon-2"
             vpcSubnetId = "subnet-058761d1c673583c8,subnet-07277bd24d3261745"
             instanceType = "t2.medium"
@@ -83,7 +83,7 @@ project {
             instanceTags = mapOf(
                 "Owner" to "evie.rocha@jetbrains.com"
             )
-            source = Source("ami-0bf5a1ee8ed002b22")
+            source = Source("ami-09c358ba71fe4ee8b")
         }
         activeStorage {
             id = "PROJECT_EXT_6"
