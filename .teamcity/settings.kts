74a5e438607d384d7b0082455dd7c27203a046f5
admin <admin@TeamCity>
2025-01-20 17:31:29 +0100

    Updated cloud profile 'amazon-2' in project 'project31' (TeamCity change in 'AWS Example' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -81,7 +81,7 @@ project {
         amazonEC2CloudImage {
             id = "PROJECT_EXT_9"
             profileId = "amazon-2"
-            agentPoolId = "-2"
+            agentPoolId = "21"
             name = "Agent"
             vpcSubnetId = "subnet-07277bd24d3261745"
             instanceType = "m1.medium"
