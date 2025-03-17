44cac037162d64dfea89f667111301c59c65f0cc
admin <admin@TeamCity>
2025-03-17 17:41:18 +0100

    project parameters were changed (TeamCity change in 'AWS Example' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -41,7 +41,7 @@ project {
     buildType(Build)
 
     params {
-        param("teamcity.internal.ec2.lt.amazon-2.Agent Meow.useSpotInstances", "false")
+        param("teamcity.internal.ec2.lt.amazon-2.Agent.useSpotInstances", "false")
     }
 
     features {
