26c51f64e9a3b23b5e1d324f424b9815687050de
admin <admin@TeamCity>
2025-09-25 14:46:11 +0200

    Cloud profile 'amazon-2' in project 'project31' is now disabled (TeamCity change in 'AWS Example' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -116,6 +116,7 @@ project {
         }
         amazonEC2CloudProfile {
             id = "amazon-2"
+            enabled = false
             name = "Test"
             terminateIdleMinutes = 0
             region = AmazonEC2CloudProfile.Regions.EU_WEST_DUBLIN
