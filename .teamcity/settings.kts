e4f94fb26b9f3a0490001437bc96bf0316780fa2
admin <admin@TeamCity>
2025-10-14 13:29:49 +0200

    Updated cloud profile 'amazon-2' in project 'project31' (TeamCity change in 'AWS Example' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -107,7 +107,7 @@ project {
                     {
                       "ImageId": "ami-0d5f3754bbd8ee416",
                       "KeyName": "evie-key-pair",
-                      "InstanceType": "t3.micro",
+                      "InstanceType": "t2.micro",
                       "Monitoring": {
                         "Enabled": true
                       },
