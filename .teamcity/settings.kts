a7c1d83a83e7fe618ecf597b078e0c1fbccc81d8
evie.rocha <evie.rocha@jetbrains.com>
2026-02-25 16:01:26 +0100

    Bad pool ID

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -154,7 +154,7 @@ project {
         amazonEC2CloudImage {
             id = "PROJECT_EXT_60"
             profileId = "amazon-2"
-            agentPoolId = "-2"
+            agentPoolId = "9999"
             name = "Agent"
             vpcSubnetId = "subnet-07277bd24d3261745"
             keyPairName = "evie-key-pair"
