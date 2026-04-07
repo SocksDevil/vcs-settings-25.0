c6593d329860edf5e759efb6ecbac5446ef13811
admin <admin@TeamCity>
2026-04-07 16:03:54 +0200

    build features of 'Build' build configuration were updated (TeamCity change in 'AWS Example / Subproject' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.amazonEC2CloudImage
 import jetbrains.buildServer.configs.kotlin.amazonEC2CloudProfile
+import jetbrains.buildServer.configs.kotlin.buildFeatures.provideAwsCredentials
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.projectFeatures.activeStorage
@@ -297,6 +298,12 @@ object Subproject_Build : BuildType({
 
     triggers {
         vcs {
+        }
+    }
+
+    features {
+        provideAwsCredentials {
+            awsConnectionId = "AmazonWebServicesAws_2"
         }
     }
 })
