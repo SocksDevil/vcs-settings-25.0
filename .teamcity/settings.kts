454618f1e9a58242bfe94bbc5d4537a5197b0149
admin <admin@TeamCity>
2026-04-07 16:03:41 +0200

    Connection updated (TeamCity change in 'AWS Example' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -57,6 +57,7 @@ project {
                 secretAccessKey = "credentialsJSON:4df370a7-37c3-40ec-8587-c2c14edd6993"
                 useSessionCredentials = false
             }
+            allowInSubProjects = true
             allowInBuilds = true
             stsEndpoint = "https://sts.eu-west-1.amazonaws.com"
         }
