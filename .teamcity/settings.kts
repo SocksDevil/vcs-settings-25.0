4212b58c350aba8879001c8b988b50b92ec09802
admin <admin@TeamCity>
2026-06-17 17:18:57 +0200

    Connection removed (TeamCity change in 'AWS Example' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -62,18 +62,6 @@ project {
             stsEndpoint = "https://sts.eu-west-1.amazonaws.com"
         }
         awsConnection {
-            id = "AwsExample_NewTry"
-            name = "New Try"
-            regionName = "eu-west-1"
-            credentialsType = static {
-                accessKeyId = "AKIA5JH2VERVD3VIRVGV"
-                secretAccessKey = "credentialsJSON:4df370a7-37c3-40ec-8587-c2c14edd6993"
-            }
-            allowInSubProjects = true
-            allowInBuilds = true
-            stsEndpoint = "https://sts.eu-west-1.amazonaws.com"
-        }
-        awsConnection {
             id = "AwsExample_StaticCredentialsSession"
             name = "Static Credentials Session"
             regionName = "eu-west-1"
